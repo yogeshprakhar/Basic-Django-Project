eb9e0a2fc74a594795e0395174bd050fd2b71b3c
yogeshprakhar <ygeshprakharpal200027@gmail.com>
2024-08-26 00:21:38 +0530

    basic backend setup

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id','title','content','created_at','author']
+        extra_kwargs = {'author':{"read_only":True}}
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
